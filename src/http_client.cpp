43e2d80ee42c77fcfcfe522a76f84e981099c1c3
ParsG10 <developeremre.10@gmail.com>
2020-10-20 22:26:49 +0300

    Changes.

---
+++ src/http_client.cpp
@@ -0,0 +1,8 @@
+#include <boost/asio.hpp>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+std::string get_http_data(const std::string& server, const std::string& file) {
+	
+}
--- src/youtube_dl.cpp
+++ src/youtube_dl.cpp
@@ -4,3 +4,11 @@ void Edownloader_version() {
 	std::string _version_ = "1.0.0";
 	std::cout << _version_ << std::endl;
 }
+
+void main_menu() {
+	
+}
+void download_video() {
+	std::string video_url;
+	
+}
